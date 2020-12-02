38f43f9f54bdf73fd8ece938f0c1bd20205a66b3
aleroberto <alexandreroberto2@gmail.com>
2020-12-01 21:15:05 -0300

    criacao do service
    service de foto

--- Aps/aps/src/app/fotos/fotos.page.ts
+++ Aps/aps/src/app/fotos/fotos.page.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, Injectable, OnInit } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PhotoService } from '../services/photo.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,31 +13,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class FotosPage implements OnInit {
 
-  imagem21: any = null;
-
-  constructor(private camera: Camera) { }
-
-  //camera
-  tirarFoto() {
-    console.log('kkkkkk')
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-
-    this.camera.getPicture(options).then((imageData) => {
-      let imagem = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      // Handle error
-      console.log("Erro na captura: " + err);
-    });
-  }
-
-
-
-
+  constructor(public photoService: PhotoService) { }
 
   ngOnInit() {
   }
---
+++ Aps/aps/src/app/services/photo.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+
+class Photo {
+  data: any;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  public photos: Photo[] = [];
+
+  tirarFoto() {
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.photos.unshift({
+        data: 'data:image/jpeg;base64,' + imageData
+      })
+    }, (err) => {
+      console.log("Erro na captura: " + err);
+    });
+  }
+
+
+
+  constructor(private camera: Camera) { }
+}
